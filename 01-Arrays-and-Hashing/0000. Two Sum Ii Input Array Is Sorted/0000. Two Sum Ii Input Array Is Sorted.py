74f2a017210ae8ad7e453f92b86d568ccc870225
Joshua-srm <joshuajosephjoemon@gmail.com>
2026-09-22 23:38:27 +0530

    [0000] [Time Beats: 5.71%] [Memory Beats: 6.68%] - Leet2Hub

--- 01-Arrays-and-Hashing/0000. Two Sum Ii Input Array Is Sorted/0000. Two Sum Ii Input Array Is Sorted.py
+++ 01-Arrays-and-Hashing/0000. Two Sum Ii Input Array Is Sorted/0000. Two Sum Ii Input Array Is Sorted.py
@@ -1,17 +1,14 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        
-        st=0
-        end=len(numbers)-1
-        while (st<end):
-            if numbers[st]+numbers[end]==target:
-                return [(st+1), (end+1)]
-                break
-            elif numbers[st]+numbers[end]>target:
-                end-=1
-            else:
-                st+=1
+1class Solution(object):
+2    def twoSum(self, numbers, target):
+3        l = 0
+4        r = len(numbers) - 1
+5
+6        while l < r:
+7            total = numbers[l] + numbers[r]
+8
+9            if total > target:
+10                r -= 1
+11            elif total < target:
+12                l += 1
+13            else:
+14                return [l + 1, r + 1]
